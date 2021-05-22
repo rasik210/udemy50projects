912f1180285f66279b7f8d267b473585a4da570e
Rasik <rasik210@gmail.com>
2021-05-22 22:12:23 +0530

    cross icon

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,1 +1,12 @@
+function changeButtonShape() {
+  var hambergurLayers = document.getElementsByClassName('hambergur')
+  Array.from(hambergurLayers).forEach((element) => {
+    element.classList.remove('hambergur')
+  })
 
+  var cross1 = document.getElementById('cross1')
+  cross1.classList.add('cross1')
+
+  var cross2 = document.getElementById('cross2')
+  cross2.classList.add('cross2')
+}
