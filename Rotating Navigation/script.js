4c456338787ce4c14c3587ae483d480703b081ce
Rasik <rasik210@gmail.com>
2021-05-23 01:59:39 +0530

    toggle menu button

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,12 +1,22 @@
+var isMenuVisible = 1
+
 function changeButtonShape() {
-  var hambergurLayers = document.getElementsByClassName('hambergur')
-  Array.from(hambergurLayers).forEach((element) => {
-    element.classList.remove('hambergur')
-  })
+  var menuButton = document.getElementById('menuButton')
+  var divNodes = menuButton.getElementsByTagName('div')
 
-  var cross1 = document.getElementById('cross1')
-  cross1.classList.add('cross1')
-
-  var cross2 = document.getElementById('cross2')
-  cross2.classList.add('cross2')
+  if (isMenuVisible) {
+    divNodes[0].classList.remove('hambergur')
+    divNodes[1].classList.remove('hambergur')
+    divNodes[2].classList.remove('hambergur')
+    divNodes[3].classList.add('cross1')
+    divNodes[4].classList.add('cross2')
+    isMenuVisible = 0
+  } else {
+    divNodes[0].classList.add('hambergur')
+    divNodes[1].classList.add('hambergur')
+    divNodes[2].classList.add('hambergur')
+    divNodes[3].classList.remove('cross1')
+    divNodes[4].classList.remove('cross2')
+    isMenuVisible = 1
+  }
 }
