7e40e8e93d6613f7d9c43f7dcfc8c2dc2c3567f3
Rasik <rasik210@gmail.com>
2021-06-13 15:10:26 +0530

    font awesome icons

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -2,27 +2,25 @@ var isHambergurMenuVisible = true
 var isNavBarVisible = false
 
 function changeButtonShape() {
-  const menuButton = document.getElementById('menuButton')
-  const divNodes = menuButton.getElementsByTagName('div')
+  const hambergurMenu = document.getElementById('hambergurMenu')
+  const cross = document.getElementById('cross')
   const contentDiv = document.getElementsByClassName('divCellContent')[0]
   const navBar = document.getElementById('navBar')
   const navMenu = document.getElementById('navMenu')
 
   if (isHambergurMenuVisible) {
-    divNodes[2].classList.remove('hambergur')
-    divNodes[3].classList.remove('hambergur')
-    divNodes[4].classList.remove('hambergur')
-    divNodes[0].classList.add('cross1')
-    divNodes[1].classList.add('cross2')
+    hambergurMenu.classList.add('hideMenu')
+    hambergurMenu.classList.remove('showMenu')
+    cross.classList.remove('hideCross')
+    cross.classList.add('showCross')
     contentDiv.classList.add('rotate')
     contentDiv.classList.remove('rotateBack')
     isHambergurMenuVisible = false
   } else {
-    divNodes[2].classList.add('hambergur')
-    divNodes[3].classList.add('hambergur')
-    divNodes[4].classList.add('hambergur')
-    divNodes[0].classList.remove('cross1')
-    divNodes[1].classList.remove('cross2')
+    hambergurMenu.classList.remove('hideMenu')
+    hambergurMenu.classList.add('showMenu')
+    cross.classList.add('hideCross')
+    cross.classList.remove('showCross')
     contentDiv.classList.remove('rotate')
     contentDiv.classList.add('rotateBack')
     isHambergurMenuVisible = true
