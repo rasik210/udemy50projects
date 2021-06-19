99407ae445c7fdcd3da16a3f851ef193c943bafb
Rasik <rasik210@gmail.com>
2021-06-19 11:04:53 +0530

    responsive nav menu

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,8 +1,8 @@
-var isHambergurMenuVisible = true
+var isHambergurVisible = true
 var isNavBarVisible = false
 
 function changeButtonShape() {
-  ToggleButtonShape()
+  ChangeMenuToggler()
   ToggleNavBar()
 }
 
@@ -15,46 +15,54 @@ function ToggleNavBar() {
   if (isNavBarVisible) {
     navBar.classList.add('navBarClosed')
     navBar.classList.remove('navBarOpen')
-    navMenuHome.classList.remove('list')
-    navMenuHome.classList.add('list-item-home-hide')
-    navMenuAbout.classList.remove('list')
-    navMenuAbout.classList.add('list-item-about-hide')
-    navMenuContact.classList.remove('list')
-    navMenuContact.classList.add('list-item-contact-hide')
+    HideMenu()
     isNavBarVisible = false
   } else {
     navBar.classList.remove('navBarClosed')
     navBar.classList.add('navBarOpen')
-    navMenuHome.classList.add('list')
-    navMenuHome.classList.remove('list-item-home-hide')
-    navMenuAbout.classList.add('list')
-    navMenuAbout.classList.remove('list-item-about-hide')
-    navMenuContact.classList.add('list')
-    navMenuContact.classList.remove('list-item-contact-hide')
+    ShowMenu()
     isNavBarVisible = true
   }
 }
 
-function ToggleButtonShape() {
-  const hambergurMenu = document.getElementById('hambergurMenu')
+function HideMenu() {
+  navMenuHome.classList.remove('list')
+  navMenuHome.classList.add('list-item-home-hide')
+  navMenuAbout.classList.remove('list')
+  navMenuAbout.classList.add('list-item-about-hide')
+  navMenuContact.classList.remove('list')
+  navMenuContact.classList.add('list-item-contact-hide')
+}
+
+function ShowMenu() {
+  navMenuHome.classList.add('list')
+  navMenuHome.classList.remove('list-item-home-hide')
+  navMenuAbout.classList.add('list')
+  navMenuAbout.classList.remove('list-item-about-hide')
+  navMenuContact.classList.add('list')
+  navMenuContact.classList.remove('list-item-contact-hide')
+}
+
+function ChangeMenuToggler() {
+  const hambergurButton = document.getElementById('hambergurMenu')
   const cross = document.getElementById('cross')
   const contentDiv = document.getElementsByClassName('divCellContent')[0]
 
-  if (isHambergurMenuVisible) {
-    hambergurMenu.classList.add('hideMenu')
-    hambergurMenu.classList.remove('showMenu')
+  if (isHambergurVisible) {
+    hambergurButton.classList.add('hideMenu')
+    hambergurButton.classList.remove('showMenu')
     cross.classList.remove('hideCross')
     cross.classList.add('showCross')
     contentDiv.classList.add('rotate')
     contentDiv.classList.remove('rotateBack')
-    isHambergurMenuVisible = false
+    isHambergurVisible = false
   } else {
-    hambergurMenu.classList.remove('hideMenu')
-    hambergurMenu.classList.add('showMenu')
+    hambergurButton.classList.remove('hideMenu')
+    hambergurButton.classList.add('showMenu')
     cross.classList.add('hideCross')
     cross.classList.remove('showCross')
     contentDiv.classList.remove('rotate')
     contentDiv.classList.add('rotateBack')
-    isHambergurMenuVisible = true
+    isHambergurVisible = true
   }
 }
