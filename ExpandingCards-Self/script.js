64571a9db67126389882d980c7c1f60701dfcc41
Rasik <rasik210@gmail.com>
2021-08-21 17:38:38 +0530

    picture title

--- ExpandingCards-Self/script.js
+++ ExpandingCards-Self/script.js
@@ -5,13 +5,20 @@ window.onclick = (e) => {
   if (e.target.nodeName == 'DIV' && e.target.id == '') return
 
   e.target.classList.add('expand')
-  var allDivs = document.getElementsByTagName('div')
+
+  var picTitle = e.target.getElementsByTagName('p')
+  picTitle[0].classList.add('picTitle')
+  picTitle[0].classList.remove('picTitleHide')
+
+  var allDivs = document.getElementsByClassName('panel')
 
   for (let index = 0; index < allDivs.length; index++) {
-    debugger
     const currElement = allDivs[index]
     if (currElement.id != e.target.id) {
       currElement.classList.remove('expand')
+      var childPicTitle = currElement.getElementsByTagName('p')
+      childPicTitle[0].classList.add('picTitleHide')
+      childPicTitle[0].classList.remove('picTitle')
     }
   }
 }
